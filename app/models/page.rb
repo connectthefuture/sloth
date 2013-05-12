f9116a0ca2bae7a2381a53b2d81f164895bc8950
Kevin McPhillips <github@kevinmcphillips.ca>
2013-05-12 00:11:56 -0500

    Tighten up page validations

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,5 +7,7 @@ class Page < ActiveRecord::Base
   has_attached_file :image, :styles => { :display => "1088x", :xlarge => "600x600>", :large => "400x400>", :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   validates :prototype, presence: true
+  validates :name, presence: true
+  validates :image, attachment_presence: true
 
 end
