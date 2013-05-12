5df6f845958c1b5d9ab79ed9a258bd4fd3b20ea2
Kevin McPhillips <github@kevinmcphillips.ca>
2013-05-12 12:04:03 -0500

    Add root to admin namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Sloth::Application.routes.draw do
-  root :to => "prototypes#index"
+  root to: "prototypes#index"
 
   resources :prototypes, only: [:index, :show]
 
   namespace :admin do
+    root to: "prototypes#index"
+    
     resources :prototypes do
       resources :pages do
         resources :links
@@ -12,8 +14,8 @@ Sloth::Application.routes.draw do
   end
 
   resources :users
-  match '/login', :to => 'sessions#new', :as => :login
-  match '/auth/g/callback', :to => 'sessions#create'
-  match '/auth/failure', :to => 'sessions#failure'
+  match '/login', to: 'sessions#new', as: :login
+  match '/auth/g/callback', to: 'sessions#create'
+  match '/auth/failure', to: 'sessions#failure'
 
 end
