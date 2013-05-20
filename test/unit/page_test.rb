5bb9f213e793824879adf4faea9f4692483016b2
Kevin McPhillips <github@kevinmcphillips.ca>
2013-05-20 13:17:38 -0500

    Add model tests

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -3,6 +3,16 @@ require 'test_helper'
 class PageTest < ActiveSupport::TestCase
   let(:page){ FactoryGirl.create(:page) }
   
+  test "#current_version should get the right version" do
+    highest_version = page.page_versions.build image: File.open(test_image_file)
+    page.save!
+    page.reload
+    page.current_version.must_equal(highest_version)
+  end
   
+  test "#current_version should deal with an empty set" do
+    page.page_versions.destroy_all
+    page.current_version.must_be_nil
+  end
   
 end
--- test/unit/page_version_test.rb
+++ test/unit/page_version_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class PageVersionTest < ActiveSupport::TestCase
+  let(:page){ FactoryGirl.create(:page) }
+  let(:page_version){ page.page_versions.first }
   
+  test "#current? should compare its self to the current on the page" do
+    page_version.current?.must_equal(true)
+    page_version.page.page_versions.create! image: File.open(test_image_file)
+    page_version.current?.must_equal(false)
+  end
   
 end
